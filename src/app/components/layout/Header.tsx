9c47ee72590369f62a26d05b98a616e110f4d750
Ammar Ateya <ammarat@umich.edu>
2024-10-25 12:00:00 -0400

    chore: Add keyboard shortcuts to fix edge case

--- src/app/components/layout/Header.tsx
+++ src/app/components/layout/Header.tsx
@@ -117,3 +117,4 @@ export default function Header() {
 // Updated on Tue May  6 23:08:11 EDT 2025
 // Updated on Tue May  6 23:08:12 EDT 2025
 // Updated on Tue May  6 23:08:12 EDT 2025
+// Updated on Tue May  6 23:08:12 EDT 2025
