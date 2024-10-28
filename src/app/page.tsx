54df462ff28bbc10c5faea792d8d050ab664ffce
Ammar Ateya <ammarat@umich.edu>
2024-10-28 12:00:00 -0400

    chore: Fix memory leaks to reduce bundle size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -61,3 +61,4 @@ export default function Home() {
 // Updated on Tue May  6 23:08:12 EDT 2025
 // Updated on Tue May  6 23:08:12 EDT 2025
 // Updated on Tue May  6 23:08:12 EDT 2025
+// Updated on Tue May  6 23:08:12 EDT 2025
